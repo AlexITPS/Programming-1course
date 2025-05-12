892225e7cfda575ef356857d4bd387fd392c6671
AlexITPS <yarcev1310@mail.ru>
2025-05-12 18:01:43 +0300

    Add files via upload

---
+++ LW1/_453504_Ярцев/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _453504_Ярцев
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number_1, number_2, number_3;
+            Console.WriteLine("Введите делимое: ");
+            number_1 = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Введите делитель: ");
+            number_2 = int.Parse(Console.ReadLine() ?? "0");
+
+            // Проверка на деление на ноль
+            if (number_2 == 0)
+            {
+                Console.WriteLine("Деление на ноль недопустимо.");
+                return; // Завершение программы в случае, при условии b == 0 
+            }
+
+            number_3 = Divide(number_1, number_2);
+            Console.WriteLine("Частное: ");
+            Console.WriteLine(number_3);
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
